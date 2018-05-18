4a6afce7670080158bfcd78c4a4dd50e1174c56c
Katsutoshi Seki <sekika@users.noreply.github.com>
2018-05-19 03:28:48 +0900

    0.1.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='passme',
-    version='0.1.3',
+    version='0.1.4',
     description='Password management with command line',
     long_description=long_description,
     url='https://github.com/sekika/passme',
