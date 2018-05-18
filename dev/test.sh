0ceb35bf260432b20edec0b8cc5732950320044e
Katsutoshi Seki <sekika@users.noreply.github.com>
2018-05-19 05:06:01 +0900

    Test pattern

--- dev/test.sh
+++ dev/test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Change to this directory
 cd `echo $0 | sed -e 's/[^/]*$//'`
-passme list
-passme twitter test
 echo 'flake8'
 flake8 ../passme/*.py | sed -e 's/^.*passme\///' | grep -v E501
+passme test
