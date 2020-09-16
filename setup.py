786c6301935177053d95301d346f9db222cb328e
Katsutoshi Seki <sekika@users.noreply.github.com>
2020-09-16 21:07:49 +0900

    Adding category of Python 3.8

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: OS Independent',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows :: Windows 10',
